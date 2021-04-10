d7279bebedf12864de3c0f3bdc3e2f74f2f936ee
Kyle Vincent <kyle.vinc11@gmail.com>
2021-04-11 09:31:56 +1000

    Added in Address Model

---
+++ Models/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_react_demo_backend.Models
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public int UserId { get; set; }
+        public string Street { get; set; }
+        public string Suburb { get; set; }
+        public string State { get; set; }
+        public string Postcode { get; set; }
+        public string Country { get; set; }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -7,5 +7,11 @@ namespace net_react_demo_backend.Models
 {
     public class User
     {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
     }
 }
