8a9b9cfb5b0b75b51f2da308100da2db058d5c36
Kyle Vincent <kyle.vinc11@gmail.com>
2021-04-11 13:26:07 +1000

    Added PUT, POST, and DELETE calls to user

---
+++ Controllers/AddressController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using net_react_demo_backend.Models;
+
+namespace net_react_demo_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+
+    }
+}
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
 using System.Data;
+using net_react_demo_backend.Models;
 
 namespace net_react_demo_backend.Controllers
 {
@@ -15,18 +16,30 @@ namespace net_react_demo_backend.Controllers
     public class UserController : ControllerBase
     {
         private readonly IConfiguration _configuration;
-        
+
         public UserController(IConfiguration configuration)
         {
             _configuration = configuration;
         }
 
-        [HttpGet]
-        public JsonResult Get()
+        [HttpGet("{userIdentifier}")]
+        public JsonResult Get(string userIdentifier)
         {
-            //Query to be executed
-            string query = @"
-                select UserId, FirstName, LastName, Email, Phone, Password from dbo.Users";
+            string query = @"";
+            
+            //If using email to get user
+            if (userIdentifier.Contains("@")) {
+                //Query to be executed
+                query = @"
+                select UserId, FirstName, LastName, Email, Phone, Password from dbo.Users where Email = '"+ userIdentifier +@"'";
+            }
+            //If using anything else to get user (usually ID)
+            else {
+                //Query to be executed
+                query = @"
+                select UserId, FirstName, LastName, Email, Phone, Password from dbo.Users where UserId = " + userIdentifier + @"";
+            }
+            
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("NetReactDemoCon");
             SqlDataReader myReader;
@@ -45,6 +58,90 @@ namespace net_react_demo_backend.Controllers
                 }
             }
             return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(User user)
+        {
+            //TODO: need to potentially b-crypt this password either before it's sent here, or even right here (probs before)
+            //Query to be executed
+            string query = @"
+                insert into dbo.Users values ('"+user.FirstName+ @"', '" + user.LastName + @"', '" + user.Email + @"', '" + user.Phone + @"', '" + user.Password + @"')";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("NetReactDemoCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                //Running the query here
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(User user)
+        {
+            //TODO: need to potentially b-crypt this password either before it's sent here, or even right here (probs before)
+            //Query to be executed
+            string query = @"
+                update dbo.Users set 
+                FirstName = '" + user.FirstName + @"', LastName = '" + user.LastName + @"',
+                Email = '" + user.Email + @"', Phone ='" + user.Phone + @"', Password ='" + user.Password + @"'
+                where UserID = "+ user.UserId +@"";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("NetReactDemoCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                //Running the query here
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(user.FirstName+" Was Updated Successfully");
+        }
+
+        [HttpDelete("{userId}")]
+        public JsonResult Delete(int userId)
+        {
+            //Query to be executed
+            string query = @"
+                delete from dbo.Users 
+                where UserId = "+ userId +@"";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("NetReactDemoCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                //Running the query here
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Deleted Sucessfully");
         }
     }
 }
